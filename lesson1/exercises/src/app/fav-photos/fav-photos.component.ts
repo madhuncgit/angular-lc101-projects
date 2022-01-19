a978fb266ee6bc8cccd876d19b0b0a3aa5ec4b0b
unknown <madhusureshnc@gmail.com>
2022-01-19 10:14:59 -0500

    Initial

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
-
+  photosTitle = 'Movie Posters';
+  image1 = 'https://s3.amazonaws.com/criterion-production/films/64093139506eeea73f973aee07e3f9ec/ZTvHQrwEUsJIjlvtHztiZ6g0UCH85m_large.jpg';
+  image2 = 'https://www.thesun.co.uk/wp-content/uploads/2018/06/NINTCHDBPICT000410394941.jpg?w=1980';
+  image3 = "https://lumiere-a.akamaihd.net/v1/images/p_encanto_homeent_22359_4892ae1c.jpeg";
   constructor() { }
 
   ngOnInit() {
